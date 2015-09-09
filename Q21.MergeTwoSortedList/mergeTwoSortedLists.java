ff9cd638b32e97614d3b2d0699b1192f3259b79b
J_Wang <wang006688@gmail.com>
2015-09-08 18:14:16 -0700

    An easy to understand method
    This one is pretty straightforward, simply go compare l1 and l2, determine which one is smaller and assign the node to a temp list.

---
+++ Q21.MergeTwoSortedList/mergeTwoSortedLists.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        
+        ListNode merged = new ListNode(1);
+        ListNode temp = merged;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        if (l1 != null) {
+            temp.next = l1;
+        } 
+        if (l2 != null) {
+            temp.next = l2;
+        } 
+        return merged.next;
+    }
+}
